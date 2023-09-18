acf677186333ffb8f09fb803be32aee953970f07
robertovnunes <jrvn@cin.ufpe.br>
2023-09-18 14:28:37 -0300

    Creating .env

--- conf/server.js
+++ conf/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
 
 const port = 3000;
 
@@ -11,7 +12,10 @@ app.use(
 );
 app.use(express.json());
 
-mongo = "mongodb+srv://jrvn:P3nt4t0n1x@cognitiva.ycduofb.mongodb.net/?retryWrites=true&w=majority"
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+mongo = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cognitiva.ycduofb.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(mongo)
     .then(() => {
