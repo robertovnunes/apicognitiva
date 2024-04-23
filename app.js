fbf9f1ae1933f3050c76e9c971456db820730ab4
Roberto Nunes <jrvn@cin.ufpe.br>
2024-04-23 11:26:36 -0300

    mudanças nos models e nas rotas 2

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const app = require('./conf/server');
 
 const userRouter = require('./api/routes/user');
-const rpdRouter = require('./api/routes/rpd');
-const deRouter = require('./api/routes/de');
 
 
 app.use('/users', userRouter);
-app.use('/rpd', rpdRouter);
-app.use('/de', deRouter);
 
 app.get('/', (req, res) => {
     res.send('APICognitiva está funcionando');
